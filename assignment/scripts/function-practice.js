69f6686f0026bd058883c1590d334ff8cc7f605a
ben <132373460+karuson@users.noreply.github.com>
2023-06-28 19:23:41 -0500

    find array item, not working yet check again

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -71,10 +71,17 @@ console.log('should log undefined', getLast([]));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find(value, array) {
 
-}
-
+// NOT DONE
+// function find(value, array) {
+//   for (let i=0; i<=array.length; i++) {
+//     if (array[i] === value) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } // end for of
+// }
 // ----------------------
 // Stretch Goals
 // ----------------------
